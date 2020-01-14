84b701a0d1d15cffef3116f51c5dffddd06207b3
Jacksone250 <Jacksone250@gmail.com>
2020-01-14 11:23:39 -0700

    updates to js on index and main

--- scripts/currentDate.js
+++ scripts/currentDate.js
@@ -1,1 +1,8 @@
-let myName = 'Ethan Jackson'
+
+const currentDate = new Date();
+console.log(currentDate) 
+
+let currentYear = currentDate.getFullYear()
+console.log(currentYear)
+
+document.getElementById("currentYear").innerHTML = currentYear;
---
+++ scripts/lastUpdate.js
@@ -0,0 +1,5 @@
+const currentDate = new Date();
+
+let lastUpdateDate = currentDate.getTime;
+
+document.getElementById("lastUpdateDate").innerHTML = lastUpdateDate;
