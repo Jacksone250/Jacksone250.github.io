d3575609ac09a3266343e9cd9613520c248793ba
Jacksone250 <Jacksone250@gmail.com>
2020-02-07 20:46:38 -0700

    added active class

--- lesson5/scripts/new.js
+++ lesson5/scripts/new.js
@@ -15,3 +15,17 @@ else {
 }
 
 
+// attaching active class to active links
+const navBar = document.querySelector('.navigation');
+const links = navBar.getElementsByClassName('lnk');
+
+for (let i = 0; i < lnks.length; i++) {
+    lnks[i].addEventListener("click", function(){
+    let current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+    });
+}
+
