b1029bf5b30ed78ec2f2fcc448d9b9fdf77cb80f
Jacksone250 <Jacksone250@gmail.com>
2020-02-11 10:31:37 -0700

    lesson 6 creation

---
+++ lesson6/scripts/dateDMY.js
@@ -0,0 +1,32 @@
+/* Wednesday, 20 May 2020 */ 
+
+let fullDate;
+
+// Get date variable 
+const currentDate = new Date(); 
+
+// Get day of week 
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 
+                    'Wednesday', 'Thursday', 'Friday', 
+                    'Saturday'];
+const day = daysOfWeek[currentDate.getDay()];
+
+// Get day of Month 
+const dayOfMonth = currentDate.getDate();
+
+// Get  month 
+const months = ['January', 'February', 'March', 'April', 
+                'May', 'June', 'July', 'August', 
+                'September', 'October', 'November', 
+                'December']
+const month = months[currentDate.getMonth()];
+
+// Get year
+let year = currentDate.getFullYear();
+
+// Full date 
+fullDate = day + ', ' + dayOfMonth + ' ' + month + ' ' + year;
+
+//document.getElementById('currentDate').innerHTML = fullDate;
+// or....
+document.querySelector('#currentDate').textContent = fullDate;
---
+++ lesson6/scripts/new.js
@@ -0,0 +1,31 @@
+// weather summary update
+document.querySelector('#current-temp').innerHTML = '50&deg;'
+document.querySelector('#wind-speed').innerHTML = '5mph'
+document.querySelector('#humidity').innerHTML = '30%'
+
+// aside pancakse at the park 
+const checkDay = new Date();
+const aside = document.querySelector('aside');
+
+if ( checkDay.getDay() !== 5 ) {
+    aside.style.display = 'none';
+}
+else {
+    aside.style.display = 'block';
+}
+
+
+// attaching active class to active links
+const navBar = document.querySelector('.navigation');
+const links = navBar.getElementsByClassName('lnk');
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function(){
+    let current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+    });
+}
+
