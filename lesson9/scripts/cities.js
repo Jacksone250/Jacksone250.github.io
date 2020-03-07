7880ef126aa115c5192fcff27b726a6aa9387dd3
Jacksone250 <Jacksone250@gmail.com>
2020-03-07 08:48:52 -0700

    lesson 9

--- lesson9/scripts/cities.js
+++ lesson9/scripts/cities.js
@@ -18,18 +18,18 @@ fetch('https://byui-cit230.github.io/weather/data/towndata.json')
             else if (town.name === 'Preston') {
                 document.querySelector('#pName').textContent = town.name;
                 document.querySelector('#pMotto').textContent = town.motto;
-                document.querySelector('#pYear').textContent = town.yearFounded;
-                document.querySelector('#pPop').textContent = town.currentPopulation;
-                document.querySelector('#pRain').textContent = town.averageRainfall;
+                document.querySelector('#pYear').textContent = `Year Founded:  ${town.yearFounded}`;
+                document.querySelector('#pPop').textContent = `Population: ${town.currentPopulation}`;
+                document.querySelector('#pRain').textContent = `Annual Rain Fall: ${town.averageRainfall}`;
                 document.querySelector('#pImg').setAttribute('src', `images/${town.photo}`);
                 document.querySelector('#pImg').setAttribute('alt', 'Picture of the Preston');
             }
             else if (town.name === 'Soda Springs') {
                 document.querySelector('#ssName').textContent = town.name;
                 document.querySelector('#ssMotto').textContent = town.motto;
-                document.querySelector('#ssYear').textContent = town.yearFounded;
-                document.querySelector('#ssPop').textContent = town.currentPopulation;
-                document.querySelector('#ssRain').textContent = town.averageRainfall;
+                document.querySelector('#ssYear').textContent = `Year Founded:  ${town.yearFounded}`;
+                document.querySelector('#ssPop').textContent = `Population: ${town.currentPopulation}`;
+                document.querySelector('#ssRain').textContent = `Annual Rain Fall: ${town.averageRainfall}`;
                 document.querySelector('#ssImg').setAttribute('src', `images/${town.photo}`);
                 document.querySelector('#ssImg').setAttribute('alt', 'Picture of the Soda Springs');
             }
