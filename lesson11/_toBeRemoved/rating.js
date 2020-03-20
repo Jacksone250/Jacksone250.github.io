61ce9db5a89edf3a98334b875857c518d70b6a24
Jacksone250 <Jacksone250@gmail.com>
2020-03-20 13:02:12 -0600

    stormcenter reorg rating depricated moved depricated files

---
+++ lesson11/_toBeRemoved/rating.js
@@ -0,0 +1,7 @@
+
+// PLEASE NOTE: This file should no longer be in use trouble shoot removing it when ready to begin removing files.
+
+// specifically for adjusting the rating in the storm center
+// function adjustRating(rating) {
+//     document.getElementById('rating').textContent = rating;
+// }
--- lesson11/scripts/rating.js
+++
@@ -1,5 +0,0 @@
-
-// specifically for adjusting the rating in the storm center
-function adjustRating(rating) {
-    document.getElementById('rating').textContent = rating;
-}
